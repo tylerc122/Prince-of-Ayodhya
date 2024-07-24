6fa992e81a173b69af41af2b8857f865b6e8ad40
VeerMal <veer.r.malhotra@gmail.com>
2024-07-24 14:20:31 -0400

    Added stamina bar

--- UImanager.cs
+++ UImanager.cs
@@ -5,26 +5,39 @@ public partial class UImanager : CanvasLayer
 {
 
 	private ProgressBar health;
-	public int value;
+	private ProgressBar stamina;
+	public int healthValue;
+	public int staminaValue;
 	private Ram ram;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		health = GetNode<ProgressBar>("HealthBar");
+		stamina = GetNode<ProgressBar>("StaminaBar");
 		
 		// BULL SHIT I HAVE NO IDEA WHY IT WAS SO HARD I HATE GODOT
 		ram= GetNode<Ram>("../CharacterBody2D");
 
 		ram.TakeDamage(10);
 	}
+	//updates health bar to match player health
+	public void updateHealth(){
+
+		healthValue = ram.currentHealth;
+		health.Value = healthValue;
+	}
+	//updates stamina bar to match player stamina
+	public void updateStamina(){
+		staminaValue = ram.currentStamina;
+		stamina.Value = staminaValue;
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//update value to rams health at every frame
-		value = ram.Health();
-		//update health bar to match health every frame
-		health.Value = value;
+		updateHealth();
+		updateStamina();
+
 	}
 }
