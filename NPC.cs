681fe596117c9443aba873824413b04c05817e25
tylerc122 <tylercollo1@gmail.com>
2024-08-04 14:52:17 -0400

    Finished NPC logic.

--- NPC.cs
+++ NPC.cs
@@ -12,27 +12,68 @@ public partial class NPC : CharacterBody2D
 
 	public override void _Ready()
 	{
-		GetNode<CollisionShape2D>("CollisionShape2D").Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
-		GetNode<CollisionShape2D>("CollisionShape2D").Connect("body_exited", new Callable(this, nameof(OnBodyExited)));
+		var area = GetNode<Area2D>("Area2D");
+
+		area.Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
+
+		area.Connect("area_exited", new Callable(this, nameof(OnAreaExited)));
+
 
 		dialogueLabel = GetNode<Label>("Label");
-		dialogueLabel.Visible = false;
-	}
 
-	private void OnBodyEntered(Node body)
+		dialogueLabel.Visible = false;
+
+		GD.Print("NPC Ready. Dialogue label initialized.");
+	}
+	public override void _Input(InputEvent @event)
 	{
-		if (body is Ram)
+		GD.Print($"Player in range: {playerInRange}");
+		if (@event.IsActionPressed("interact") && playerInRange)
 		{
-			playerInRange = true;
+			GD.Print("Interaction triggered. Showing next dialogue.");
 			ShowNextDialogue();
 		}
 	}
-	private void OnBodyExited()
-	{
 
+	private void OnAreaEntered(Area2D area)
+	{
+		GD.Print($"Something entered: {area.Name} of type {area.GetType()}");
+		var parent = area.GetParent();
+		GD.Print($"Parent node: {parent.Name}, Type: {parent.GetType()}");
+
+		if (parent is Ram)
+		{
+			GD.Print("Ram entered the area.");
+			playerInRange = true;
+			dialogueLabel.Visible = true;
+			dialogueLabel.Text = "[Press 'E' to interact]";
+		}
+		else
+		{
+			GD.Print($"Entered area's parent is not of type Ram: {parent.GetType()}");
+		}
+	}
+	private void OnAreaExited(Area2D area)
+	{
+		var parent = area.GetParent();
+		if (parent is Ram)
+		{
+			playerInRange = false;
+			dialogueLabel.Visible = false;
+			GD.Print("Ram exited NPC range.");
+		}
 	}
 	private void ShowNextDialogue()
 	{
-
+		if (dialogueIndex < Dialogues.Length)
+		{
+			dialogueLabel.Text = Dialogues[dialogueIndex];
+			dialogueIndex++;
+		}
+		else
+		{
+			dialogueLabel.Text = "";
+			dialogueIndex = 0;
+		}
 	}
 }
