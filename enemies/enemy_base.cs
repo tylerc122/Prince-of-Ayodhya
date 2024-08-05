295c2ae1e3a751e2f7736b6273b5269cd7baec8e
tylerc122 <tylercollo1@gmail.com>
2024-08-05 17:46:01 -0400

    Updated comments.

--- enemies/enemy_base.cs
+++ enemies/enemy_base.cs
@@ -2,15 +2,20 @@ using Godot;
 
 public abstract partial class EnemyBase : CharacterBody2D
 {
+    // Enemy stats
     public int health = 100;
     public int speed = 200;
     public int damage = 10;
 
+    // Initialize target
     protected Ram target;
+
+    // Start off with not being able to attack
     protected bool isAttacking = false;
 
     public override void _Ready()
     {
+        // Get target, right now only getting ram as the target.
         target = GetParent().GetNode<Ram>("Ram");
     }
 
--- enemies/enemy_bow.cs
+++ enemies/enemy_bow.cs
@@ -2,41 +2,56 @@ using Godot;
 
 public partial class EnemyBow : EnemyBase
 {
+    // Scene variable(s).
     public PackedScene ArrowScene;
-    public float attackCooldown = 2.0f;
 
+    // Attack cooldown variables.
+    public float attackCooldown = 2.0f;
     private Timer attackCooldownTimer;
+
+    // State variable(s)
     private bool canAttack = true;
 
     public override void _Ready()
     {
+        // Initialize attackCooldownTimer
+        // This includes making it oneshot, setting up wait time, & connecting timeout to specialized method.
         attackCooldownTimer = GetNode<Timer>("AttackCooldownTimer");
         attackCooldownTimer.WaitTime = attackCooldown;
         attackCooldownTimer.OneShot = true;
         attackCooldownTimer.Connect("timeout", new Callable(this, nameof(OnAttackCooldownTimeout)));
 
+        // As long as the arrow scene isn't initialzed falsely.
         if (ArrowScene == null)
         {
+            // Initialize it.
             ArrowScene = (PackedScene)ResourceLoader.Load("res://arrow.tscn");
         }
     }
 
+
     public override void _PhysicsProcess(double delta)
     {
+        // If target is initialized and able to attack.
         if (target != null && canAttack)
         {
+            // Call Attack().
             Attack();
         }
     }
 
     protected void Attack()
     {
+        // If ArrowScene is initialized properly and attack cooldown timer isn't running.
         if (ArrowScene != null && canAttack)
         {
+            // Initialize arrowInstance to an instance of our arrow scene.
             var arrowInstance = (Arrow)ArrowScene.Instantiate();
             GetParent().AddChild(arrowInstance);
+            // Initializes the arrow instance at the position of the bow enemy.
             arrowInstance.Initialize(GlobalPosition, target.GlobalPosition);
 
+            // Start the attack cooldown timer and set canAttack to false.
             canAttack = false;
             attackCooldownTimer.Start();
         }
@@ -44,6 +59,7 @@ public partial class EnemyBow : EnemyBase
 
     private void OnAttackCooldownTimeout()
     {
+        // Set canAttack to true on time out of attack cooldown timer.
         canAttack = true;
     }
 }
