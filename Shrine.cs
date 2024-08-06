55da3e30b9c10bf7b28dd9273b8bc0db6ba78d2e
tylerc122 <tylercollo1@gmail.com>
2024-08-06 12:03:44 -0500

    Started updating comments.

--- Shrine.cs
+++ Shrine.cs
@@ -3,21 +3,29 @@ using System;
 
 public partial class Shrine : Node2D
 {
+	// Ideally we should have three powerup options per shrine.
 	private PowerUp[] powerUpOptions = new PowerUp[3];
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
+		// I don't believe these should happen on ready unless we plan to instantiate the shrine when the level ends.
+		// Not even sure how ready works on instantiated objects, we'll see.
 		GeneratePowerUpOptions();
 		DisplayPowerUpOptions();
 	}
 
+	// Handles generating the randomized power ups.
 	private void GeneratePowerUpOptions()
 	{
+		// Get assign powerupmanager node.
 		PowerUpManager powerUpManager = GetNode<PowerUpManager>("../PowerUpManager");
+		// Assign random var.
 		Random random = new Random();
 
+		// For loop to assign what power ups will be displayed.
 		for (int i = 0; i < powerUpOptions.Length; i++)
 		{
+			// Our index is a random number picked from any number from 1 to n where n is the total count of all temporary power ups.
 			int index = random.Next(powerUpManager.allTemporaryPowerUps.Count);
 			powerUpOptions[i] = powerUpManager.allTemporaryPowerUps[index];
 		}
