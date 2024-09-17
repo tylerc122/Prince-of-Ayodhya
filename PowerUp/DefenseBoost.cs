dfeeb1dfeb375c53ff06831907598c4a9ff4414d
tylerc122 <tylercollo1@gmail.com>
2024-09-17 10:00:33 -0400

    Don't want to work on boss, refining power up system

--- PowerUp/DefenseBoost.cs
+++ PowerUp/DefenseBoost.cs
@@ -7,22 +7,16 @@ public partial class DefenseBoost : PowerUp
     public DefenseBoost()
     {
         PowerUpName = "Defense Boost";
-
-        // Either we need to specify that it's only for the run or have some distinction visually.
         Description = "Decreases damage taken by 5";
+        isTemporary = true;
     }
 
     public override void Apply(Ram ram)
     {
-
-    }
-
-    public override bool isTemporary()
-    {
-        throw new System.NotImplementedException();
+        ram.defense += defenseBoost;
     }
     public override void RemoveEffect(Ram ram)
     {
-        throw new System.NotImplementedException();
+        ram.defense -= defenseBoost;
     }
 }
--- PowerUpManager.cs
+++ PowerUpManager.cs
@@ -1,45 +1,50 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class PowerUpManager : Node
 {
 	private Ram ram;
-	private List<PowerUp> temporaryPowerUps = new List<PowerUp>();
+	public List<PowerUp> activePowerUps = new List<PowerUp>();
+	public List<PowerUp> allPermanentPowerUps = new List<PowerUp>();
 	public List<PowerUp> allTemporaryPowerUps = new List<PowerUp>();
 	public override void _Ready()
 	{
 		ram = GetNode<Ram>("../Ram");
+		InitializePowerUps();
+
+	}
+
+	public void InitializePowerUps()
+	{
 		allTemporaryPowerUps.Add(new DamageBoost());
 		allTemporaryPowerUps.Add(new SpeedBoost());
 		allTemporaryPowerUps.Add(new DefenseBoost());
-
 	}
 	public void ApplyPowerUp(PowerUp powerUp)
 	{
 		powerUp.Apply(ram);
-
-		if (powerUp.isTemporary())
-		{
-			temporaryPowerUps.Add(powerUp);
-		}
-		else
-		{
-			// Not sure how to implement perm power ups yet
-		}
+		activePowerUps.Add(powerUp);
 	}
 
 	public void OnDeath()
 	{
-		foreach (var powerUp in temporaryPowerUps)
+		foreach (var powerUp in activePowerUps.ToList())
 		{
-			powerUp.RemoveEffect(ram);
+			if (powerUp.isTemporary)
+			{
+				powerUp.RemoveEffect(ram);
+				activePowerUps.Remove(powerUp);
+			}
 		}
-		temporaryPowerUps.Clear();
 	}
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		foreach (var powerUp in activePowerUps)
+		{
+			powerUp.Apply(ram);
+		}
 	}
 }
